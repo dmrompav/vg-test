295e5900df2716a72f25046673dc9b409b37f5ab
dmjsdev <dm.js.dev@gmail.com>
2025-12-29 12:17:21 +0500

    fontWeight fix

--- src/main.tsx
+++ src/main.tsx
@@ -4,6 +4,7 @@ import { createRoot } from "react-dom/client";
 import "normalize.css";
 
 import "@fontsource/inter/400.css";
+import "@fontsource/inter/500.css";
 import "@fontsource/inter/600.css";
 import "@fontsource/inter/700.css";
 
